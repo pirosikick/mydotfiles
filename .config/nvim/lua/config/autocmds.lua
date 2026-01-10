f21a967efa22a962def4f5675029ca93669f3fce
Hiroyuki Anai <pirosikick@gmail.com>
2026-01-11 00:06:58 +0900

    add .config/nvim

---
+++ .config/nvim/lua/config/autocmds.lua
@@ -0,0 +1,24 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+--
+-- Add any additional autocmds here
+-- with `vim.api.nvim_create_autocmd`
+--
+-- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
+-- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- IME auto-off when leaving insert mode
+vim.api.nvim_create_autocmd("InsertLeave", {
+  pattern = "*",
+  callback = function()
+    vim.fn.system("im-select com.apple.keylayout.ABC")
+  end,
+})
+
+-- IME auto-off when leaving terminal mode
+vim.api.nvim_create_autocmd("TermLeave", {
+  pattern = "*",
+  callback = function()
+    vim.fn.system("im-select com.apple.keylayout.ABC")
+  end,
+})
